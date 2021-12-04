f503a0a60020ddc643875f2fb7aba68eb2eb5a46
celmartinez <cmartine@student.42.fr>
2021-12-04 13:18:05 +0100

    fetch bookmark metadata when submit form

--- technical-test/src/components/AddBookmarkForm/AddBookMarkForm.tsx
+++ technical-test/src/components/AddBookmarkForm/AddBookMarkForm.tsx
@@ -1,24 +1,59 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 
 const AddBookMarkForm = () => {
-    const [inputValue, setinputValue] = useState("");
+    const [inputValue, setInputValue] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        if (error) setError("");
+    }, [inputValue]);
+
+    const fetchBookmarkMetadata = async () => {
+        setIsLoading(true);
+        setInputValue("");
+        try {
+            const response = await fetch(
+                `http://noembed.com/embed?url=${inputValue}`
+            );
+            const bookmarkMetaData = await response.json();
+            console.log("bookmarkMetaData :>> ", bookmarkMetaData);
+            if (bookmarkMetaData.error) setError(bookmarkMetaData.error);
+            return bookmarkMetaData;
+        } catch (err) {
+            console.error(err);
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>) =>
-        setinputValue(event.target.value);
+        setInputValue(event.target.value);
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-        // TODO: fetch bookmark metadata here  https://noembed.com/
+        if (inputValue) fetchBookmarkMetadata();
         event.preventDefault();
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Add a bookmark:
-                <input type="text" value={inputValue} onChange={handleChange} />
-            </label>
-            <input type="submit" value="Envoyer" />
-        </form>
+        <>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Add a bookmark:
+                    <input
+                        type="text"
+                        value={inputValue}
+                        onChange={handleChange}
+                    />
+                </label>
+                <input
+                    type="submit"
+                    disabled={isLoading || !inputValue}
+                    value="Go"
+                />
+            </form>
+            {error}
+        </>
     );
 };
 
